65c4d0d56abb2c186b894d40de22f165257063a1
leoshen <leoshen@cloudmosa.com>
2022-03-20 21:47:17 +0800

    Ensure aigis chars are sorted in correct order
    If rarities are same, use classes as fallback, and vice versa.

--- src/resources/aigis/sorters.tsx
+++ src/resources/aigis/sorters.tsx
@@ -30,11 +30,30 @@ function classIndexer(c: any) {
   return classToIdx[c.class];
 }
 
-function createCompareFunc(indexer: (c: any) => number, multiplier: number) {
+// Not in use
+/*function createCompareFunc(indexer: (c: any) => number, multiplier: number) {
   return function cmp(c1: any, c2: any) {
     const i1 = indexer(c1);
     const i2 = indexer(c2);
     return i1 !== i2 ? multiplier * (i1 - i2) : c1.id - c2.id;
+  };
+}*/
+
+function createCompareFuncWithFallback(
+  indexer: (c: any) => number,
+  multiplier: number,
+  fallbackIndexer: (c: any) => number
+) {
+  return function cmp(c1: any, c2: any) {
+    const i1 = indexer(c1);
+    const i2 = indexer(c2);
+    if (i1 !== i2) return multiplier * (i1 - i2);
+
+    const fi1 = fallbackIndexer(c1);
+    const fi2 = fallbackIndexer(c2);
+    if (fi1 !== fi2) return fi1 - fi2;
+
+    return c1.id - c2.id;
   };
 }
 
@@ -43,25 +62,25 @@ const sorters = [
     id: "rarity_desc",
     name: "★▼",
     color: "#af5f00",
-    func: createCompareFunc(rarityIndexer, -1),
+    func: createCompareFuncWithFallback(rarityIndexer, -1, classIndexer),
   },
   {
     id: "rarity_asc",
     name: "★▲",
     color: "#af5f00",
-    func: createCompareFunc(rarityIndexer, 1),
+    func: createCompareFuncWithFallback(rarityIndexer, 1, classIndexer),
   },
   {
     id: "class_desc",
     name: "クラス▼",
     color: "#af0000",
-    func: createCompareFunc(classIndexer, 1),
+    func: createCompareFuncWithFallback(classIndexer, 1, rarityIndexer),
   },
   {
     id: "class_asc",
     name: "クラス▲",
     color: "#af0000",
-    func: createCompareFunc(classIndexer, -1),
+    func: createCompareFuncWithFallback(classIndexer, -1, rarityIndexer),
   },
 ];
 
